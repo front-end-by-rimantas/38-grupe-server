87e822bf6f6c01762fece5859a1d4f5668e2c63c
Rimantas <belauzas@yahoo.com>
2022-06-15 19:30:47 +0300

    Page class improvement

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,13 @@
 import { PageTemplate } from "../lib/PageTemplate.js";
 
 class PageHome extends PageTemplate {
+    constructor() {
+        super();
+    }
 
+    mainHTML() {
+        return `<h1>Home page 🎅</h1>`;
+    }
 }
 
 export { PageHome };
