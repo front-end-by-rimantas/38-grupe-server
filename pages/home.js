22007e089121c3ab4faef38c88e625fb0d2d6fde
Rimantas <belauzas@yahoo.com>
2022-06-16 19:17:54 +0300

    User logged in state

--- pages/home.js
+++ pages/home.js
@@ -1,8 +1,8 @@
 import { PageTemplate } from "../lib/PageTemplate.js";
 
 class PageHome extends PageTemplate {
-    constructor() {
-        super();
+    constructor(data) {
+        super(data);
     }
 
     mainHTML() {
