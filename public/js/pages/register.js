be2bf06f2aaa1a2fd10f912883c836d4a664dfaa
Rimantas <belauzas@yahoo.com>
2022-06-16 21:32:19 +0300

    Register form elements

--- public/js/pages/register.js
+++ public/js/pages/register.js
@@ -1,1 +1,11 @@
-console.log('Labas rytas is REGISTER 🎅');
+const formDOM = document.querySelector('.form');
+const inputsDOM = formDOM.querySelectorAll('input');
+const submitDOM = formDOM.querySelector('button');
+
+if (submitDOM) {
+    submitDOM.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log('SIUNCIAM INFO....');
+        console.log(inputsDOM);
+    })
+}
