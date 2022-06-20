2889bba6b45bee2057714761c02735bb08fc065e
Rimantas <belauzas@yahoo.com>
2022-06-20 19:42:24 +0300

    Getting info from form

---
+++ public/js/components/IsValid.js
@@ -0,0 +1,15 @@
+class IsValid {
+    static fullname() {
+        return [true, 'fullname'];
+    }
+
+    static email() {
+        return [true, 'email'];
+    }
+
+    static password() {
+        return [true, 'password'];
+    }
+}
+
+export { IsValid }
--- public/js/pages/register.js
+++ public/js/pages/register.js
@@ -1,11 +1,46 @@
+import { IsValid } from "../components/IsValid.js";
+
 const formDOM = document.querySelector('.form');
 const inputsDOM = formDOM.querySelectorAll('input');
 const submitDOM = formDOM.querySelector('button');
+const notificationsDOM = formDOM.querySelector('.notifications');
 
 if (submitDOM) {
     submitDOM.addEventListener('click', (e) => {
         e.preventDefault();
-        console.log('SIUNCIAM INFO....');
-        console.log(inputsDOM);
+
+        const data = {};
+
+        for (const inputDOM of inputsDOM) {
+            if (inputDOM.type !== 'checkbox') {
+                data[inputDOM.name] = inputDOM.value;
+                const validationRule = inputDOM.dataset.validation;
+                const validationResult = IsValid[validationRule]();
+                console.log(validationResult);
+            } else {
+                data[inputDOM.name] = inputDOM.checked;
+            }
+        }
+
+        console.log(data);
+
+        // tikriname ar laukai ne tusti
+        // tikriname ar geros vertes:
+        //      ar vardas "tinkamas" (minimum 2 zodziai)
+        //      ar email "tinkamas"
+        //      ar password "tinkamas"
+        //      ar repeat-password "tinkamas"
+        //      ar password === repeat-password
+        //      ar TOS pazymetas (check)
+        //  JEI yra klaidu:
+        //      klaidas atvaizduojame pranesimu bloke
+        //  JEI klaidu nera:
+        //      sekmes pranesima atvaizduojame pranesimu bloke
+
+        // siunciam i backend'a (API)
+        //      JEI grazino klaida
+        //          klaidas atvaizduojame pranesimu bloke
+        //      JEI klaidu nera:
+        //          sekmes pranesima atvaizduojame pranesimu bloke
     })
 }
